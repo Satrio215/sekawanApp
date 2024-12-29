4af56a28096b47ac23dc6f2b297f7af93d043eb3
Satrio215 <satrio.ajinoto@gmail.com>
2024-12-30 00:09:12 +0700

    sekawanApp

---
+++ database/migrations/2024_12_29_164009_create_pemesanans_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('pemesanans', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('id_kendaraan');
+            $table->unsignedBigInteger('id_user');
+            $table->unsignedBigInteger('id_penyetuju');
+            $table->unsignedBigInteger('id_supir');
+            $table->enum('status_user',['disetujui','ditolak','proses']);
+            $table->enum('status_penyetuju', ['disetujui','ditolak','proses']);
+            $table->string('nama_pemesan');
+            $table->date('hari');
+            $table->string('telp_pemesan');
+            $table->foreign('id_kendaraan')->references('id')->on('kendaraans')->onDelete('cascade');
+            $table->foreign('id_user')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('id_penyetuju')->references('id')->on('penyetujus')->onDelete('cascade');
+            $table->foreign('id_supir')->references('id')->on('supirs')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('pemesanans');
+    }
+};
